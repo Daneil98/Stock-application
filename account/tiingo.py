8b393c476fea3d2d92a382cb6fd3a0e59f7456e0
Daniel Idahosa <75020969+Daneil98@users.noreply.github.com>
2025-01-15 01:21:16 +0100

    Update tiingo.py

--- account/tiingo.py
+++ account/tiingo.py
@@ -6,9 +6,9 @@ headers = {
     }
 
 def get_meta_data(ticker):
-    requestResponse = requests.get("https://api.tiingo.com/tiingo/daily/{}?token=ade8eea34c1658569b3997046a22003af1a3ad08".format(ticker), headers=headers)
+    requestResponse = requests.get("https://api.tiingo.com/tiingo/daily/{}?token=   ".format(ticker), headers=headers)
     return requestResponse.json()
 
 def get_price(ticker):
-    requestResponse = requests.get("https://api.tiingo.com/tiingo/daily/{}/prices?token=ade8eea34c1658569b3997046a22003af1a3ad08".format(ticker), headers=headers)
+    requestResponse = requests.get("https://api.tiingo.com/tiingo/daily/{}/prices?token=   ".format(ticker), headers=headers)
     return requestResponse.json()[0]
