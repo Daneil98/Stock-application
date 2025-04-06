3ca3f9829afe83453cce2285732f33fa51cd3017
Daniel Idahosa <75020969+Daneil98@users.noreply.github.com>
2025-04-06 17:14:52 +0100

    Update views.py

--- payment/views.py
+++ payment/views.py
@@ -24,7 +24,7 @@ gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
 def payment_process(request):
     
     Payment_id = request.session.get('Payment_id')
-    name = request.user.username
+    user_name = request.user.username
     
     if request.method == 'POST':
         # retrieve nonce
@@ -57,7 +57,28 @@ def payment_process(request):
         # create and submit transaction
         if result and result.is_success:
 
-            payment(name, result)                 # A celery task
+            if not Wallet.objects.filter(user=user_name).exists() and not Payment.objects.filter(user=user_name).exists():
+                Wallet.objects.create(user = user_name, balance = amount, stock_eq = 0.00)
+                Payment.objects.create(user = user_name, amount = amount, paid = True, braintree_id = result.transaction.id)
+            
+            else:    
+                Payment_instance = Payment.objects.filter(user=user_name).last()
+                Payment_instance.user = user_name
+                Payment_instance.amount += amount
+                # mark the order as paid
+                Payment_instance.paid = True
+                # store the unique transaction id
+                Payment_instance.braintree_id = result.transaction.id
+                
+                
+                Wallet_instance = Wallet.objects.filter(user=user_name).last()
+                Wallet_instance.balance += amount
+                Wallet_instance.user = user_name
+                
+                # save the payment
+                Payment_instance.save()
+                Wallet_instance.save()
+                   
             
             return redirect('payment:done')
         else:
@@ -82,7 +103,7 @@ def payment_canceled(request):
 @login_required
 def stock_buy(request):
     form = BuyForm(request.POST)
-    sign = True                                     # Helps determine what kind of operation is ongoing
+#    sign = True                                     # Helps determine what kind of operation is ongoing
     
     #GETS THE USER'S DATA
     user = request.user
@@ -91,8 +112,9 @@ def stock_buy(request):
     totp = pyotp.TOTP(profile.secret_key)
     
     # PRICE FETCHING
-    balance = get_wallet_data(name)         #Balance left in the account
-    data = get_latest_price(name)           #Gets the most recent price entry in the price_db database 
+    balance = Wallet.objects.filter(user=name).last()       #Balance left in the account
+    data = price_db.objects.filter(user=name).last()            #Gets the most recent price entry in the price_db database as a queryset
+
     
     #DATA EXTRACTION
     prices = float(data.closeprice)                                   #Strips the close-price from the price_db and declares it as a float data type so it can be used in arithmetic operations
@@ -101,6 +123,9 @@ def stock_buy(request):
     wallet_balance = float(balance.balance)                        #Assigns the wallet balance as a float data type 
     stock_eq = float(balance.stock_eq)                       #Current stock equity in the wallet assigned
 
+    amount1 = float(balance.balance)     #Assigns the wallet balance as a float data type 
+    amount3 = float(balance.stock_eq)    #Current stock equity in the wallet assigned
+    amount2 = float(prices)             #Current price of the stock to be purchased
     
     if request.method == "POST":
         if form.is_valid():
@@ -113,17 +138,41 @@ def stock_buy(request):
             shares_bought = float(sum_price / prices)
             
             if totp.verify(otp):
-                wallet = Wallet.objects.filter(user=name).last()
-                update_stock_wallet(name, stock_name, shares_bought, sum_price, sign)               #celery task updates the user's stock wallet
+                if not Stock_Wallet.objects.filter(user=name).exists():
+                    shares =  0   
+                    stock_equity = 0
+                    
+                    StockHoldingTransaction_instance = StockHolding(amount1, stock_equity)
+                    Stock_Wallet.objects.create(user = name, name = stock_name,
+                        shares = StockHoldingTransaction_instance.buy_shares(shares, shares_bought),            #Calculate the number of shares added and update it 
+                        equity = StockHoldingTransaction_instance.buy_stock_eq_update(sum_price)               #Calculate the amount of stock equity added and update it 
+                    )
+                     
+                else:
+                    StockWallet_instance = Stock_Wallet.objects.get(name=stock_name)
+                    stock_equity = StockWallet_instance.equity
+                    StockHoldingTransaction_instance = StockHolding(amount1, stock_equity)
+                    #StockWallet_instance.ticker = ticker
+                    StockWallet_instance.user = name
+                    StockWallet_instance.name = stock_name
+                    shares =  StockWallet_instance.shares               #stocks shares present before current transaction 
+                    StockWallet_instance.shares = StockHoldingTransaction_instance.buy_shares(shares, shares_bought)            #Calculate the number of shares added and update it 
+                    StockWallet_instance.equity = StockHoldingTransaction_instance.buy_stock_eq_update(sum_price)               #Calculate the amount of stock equity added and update it 
+                    StockWallet_instance.save(commit=False)
+                    
+                    
+                WalletTransaction_instance = WalletTransaction(amount1, amount3)
+                Wallet_instance = Wallet.objects.filter(user=name).last()
+                Wallet_instance.user = name
+                Wallet_instance.balance = WalletTransaction_instance.buy_balance_update(sum_price)      
+                Wallet_instance.stock_eq = WalletTransaction_instance.buy_stock_eq_update(sum_price)    
+                Wallet_instance.save(commit=False)
                 
-                stock_wallet, created = Stock_Wallet.objects.get_or_create(user=name, name=stock_name)
-                update_wallet(name, wallet_balance, stock_eq, sum_price, sign)                       #celery task updates the user's wallet
+                Buy.objects.create(user = name, name = stock_name, bought = True, stock_purchase_price = amount2, total_purchase_amount = sum_price, 
+                                   shares = shares_bought)            
                 
-                Buy.objects.create(user = name, name = stock_name, bought = True,                   #Documents the user's purchase
-                    stock_purchase_price = prices, total_purchase_amount = sum_price, shares = shares_bought)              
-                
-                stock_wallet.save()
-                wallet.save()
+                StockWallet_instance.save()
+                Wallet_instance.save()
                 total_amount.delete()                                                               #Deletes amount_db entry
                 data.delete()                                                                       #Deletes price_db entry
                 
@@ -142,23 +191,23 @@ def stock_sell(request):
     #GETS THE USER'S DATA
     user = request.user
     profile = get_object_or_404(Profile, user=user)
-    user_name = user.username
+    name = user_name = user.username
     totp = pyotp.TOTP(profile.secret_key)
     
     #PRICE FETCHING
-    data = get_latest_price(user_name)                #Gets the most recent price entry in the price_db database as a queryset
-    balance = get_wallet_data(user_name)              #Balance left in the account
+    data = price_db.objects.filter(user=user_name).last()                #Gets the most recent price entry in the price_db database as a queryset
+    balance = Wallet.objects.filter(user=user_name).last()             #Balance left in the account
     
     #DATA EXTRACTION
     stock_price = float(data.openprice)                             #Strips and assings the open price value from price_db as a float data type
     stock_name = str(data.name)                                     #Strips and assigns the name value as a string data type
 #   ticker = str(data.ticker)                                       #Strips and assigns the ticker as a string
-    wallet_balance = float(balance.balance)                         #Assigns the wallet balance as a float data type 
-    wallet_eq_balance = float(balance.stock_eq)           
+    amount1 = wallet_balance = float(balance.balance)      #Assigns the wallet balance as a float data type 
+    amount3 = wallet_eq_balance = float(balance.stock_eq)         
     
     
     #STOCK DATA FROM THE BUY MODEL DB
-    StockWallet_instance = get_stock_wallet(user_name, stock_name)
+    StockWallet_instance = Stock_Wallet.objects.filter(user=user_name, name=stock_name).last()
 
     if StockWallet_instance is None:
         return redirect('my_stocks')
@@ -170,23 +219,35 @@ def stock_sell(request):
                 data2.user = user_name
                 data2.save()
 
-                sum_price = float(data2.total_price)                                          # Equity user wants to sell
+                amount2 = float(data2.total_price)                                          # Equity user wants to sell
                 otp = data2.otp                                                               # otp code
                 
-                shares_sell = float(sum_price/stock_price)                                    #Calculates the number of shares to be sold
+                shares_left = float(amount2/stock_price)                                    #Calculates the number of shares to be sold
                 
                 if totp.verify(otp):
-                    update_stock_wallet(StockWallet_instance, user_name, stock_name, shares_sell, sum_price, sign)          #Updates the user's stock_wallet
-                    wallet = Wallet.objects.filter(user=user_name).last()
-                    Sell.objects.create(user = user_name, name = stock_name, #ticker=ticker,                  #Documents the user's sale
-                        sold = True, total_selling_amount = sum_price, stock_selling_price = stock_price, 
-                        shares = shares_sell)
+                    StockWallet_instance = Stock_Wallet.objects.get(name=stock_name)        #Retrieve stock wallet entry/instance by it's stock name
+                    stock_equity = StockWallet_instance.equity
+
+                    StockHoldingTransaction_instance = StockHolding(amount1, stock_equity)
+                    StockWallet_instance.user = name
+                    StockWallet_instance.name = stock_name
+                    StockWallet_instance.shares = StockHoldingTransaction_instance.sell_shares(shares_left, StockWallet_instance.shares)               #Calculate the number of shares sold and update it 
+                    StockWallet_instance.equity = StockHoldingTransaction_instance.sell_stock_eq_update(amount2)                        #Calculate the amount of stock equity sold and update it 
+                    StockWallet_instance.save(commit=False)
+                        
                     
-                    update_wallet(wallet, user_name, wallet_balance, wallet_eq_balance, sum_price, sign)      #Updates the user's wallet
+                    WalletTransaction_instance = WalletTransaction(amount1, amount3)
+                    Wallet_instance = Wallet.objects.filter(user=name).last()
+                    Wallet_instance.user = name
+                    Wallet_instance.stock_eq = WalletTransaction_instance.sell_stock_eq_update(amount2)
+                    Wallet_instance.balance = WalletTransaction_instance.sell_balance_update(amount2)
+                    Wallet_instance.save(commit=False)     #Updates the user's wallet
 
-
+                    Sell.objects.create(user = name, name = stock_name, sold = True, total_selling_amount = amount2,
+                        stock_selling_price = stock_price, shares = shares_left)
+                    
                     StockWallet_instance.save()
-                    wallet.save()
+                    Wallet_instance.save()
                     data2.delete()                      #Deletes the specific amount instance to save space
                     data.delete()
                     return redirect('payment:done')
@@ -200,7 +261,7 @@ def stock_sell(request):
 @login_required
 def long_position(request):
     form = LongForm(request.POST)
-    sign = True
+#    sign = True
     
     #GETS THE USER'S DATA
     user = request.user
@@ -209,15 +270,15 @@ def long_position(request):
     totp = pyotp.TOTP(profile.secret_key)
     
     #PRICE FETCHING
-    data = get_latest_price(user_name)                #Gets the most recent price entry in the price_db database as a queryset
-    balance = get_wallet_data(user_name)              #Balance left in the account
+    data =  price_db.objects.filter(user=user_name).last()                #Gets the most recent price entry in the price_db database as a queryset
+    balances = Wallet.objects.filter(user=user_name).last()             #Balance left in the account
     
     #DATA EXTRACTION
-    open_price = float(data.openprice)                             #Strips and assings the open price value from price_db as a float data type
+    open_price = float(data.openprice)                              #Strips and assings the open price value from price_db as a float data type
     close_price = float(data.closeprice)
     stock_name = str(data.name)                                     #Strips and assigns the name value as a string data type
     ticker = str(data.ticker)                                       #Strips and assigns the ticker as a string
-    wallet_balance = float(balance.balance)                         #Assigns the wallet balance as a float data type   
+    amount1 = float(balances.balance)                         #Assigns the wallet balance as a float data type   
 
     
     if request.method == "POST":
@@ -228,27 +289,31 @@ def long_position(request):
 
             sum_price = float(Amount.total_price)                                   # amount user wants to trade
             otp = Amount.otp                                                        # otp code
-            leverage = Amount.leverage                                              # Leverage used          
+            leverage_used = Amount.leverage                                              # Leverage used          
             
+            #Instance declaration
+            Trading_instance = Trading(amount1, leverage_used)
             
             if totp.verify(otp):
-                wallet = Wallet.objects.filter(user=user_name).last()
-                create_trade(wallet, user_name, stock_name, ticker, leverage, sum_price, close_price, open_price, balance, sign)
+                Long.objects.create(user = user_name, name = stock_name, ticker = ticker, amount = sum_price, 
+                    leverage = leverage_used, current_price = close_price, long_price = open_price, 
+                    returns = Trading_instance.long_position(sum_price, open_price, close_price))
 
-                wallet.save()
-                Amount.delete()
+                balances.balance -= sum_price
+                balances.save()
                 data.delete()
+
                 return redirect('payment:done')
             else:
                 return redirect('payment:canceled')
         
-    return render(request, 'stock_long.html', {'Sell': Sell, 'form': form,  'total': wallet_balance, 'prices': open_price, 'stock': stock_name })
+    return render(request, 'stock_long.html', {'Sell': Sell, 'form': form,  'total': balances.balance, 'prices': open_price, 'stock': stock_name })
 
 
 @login_required
 def short_position(request):
     form = ShortForm(request.POST)
-    sign = False
+#    sign = False
     
     #GETS THE USER'S DATA
     user = request.user
@@ -257,15 +322,15 @@ def short_position(request):
     totp = pyotp.TOTP(profile.secret_key)
     
     #PRICE FETCHING
-    data = get_latest_price(user_name)                #Gets the most recent price entry in the price_db database as a queryset
-    balance = get_wallet_data(user_name)              #Balance left in the account
+    data = price_db.objects.filter(user=user_name).last()                #Gets the most recent price entry in the price_db database as a queryset
+    balances = Wallet.objects.filter(user=user_name).last()              #Balance left in the account
     
     #DATA EXTRACTION
     open_price = float(data.openprice)                             #Strips and assings the open price value from price_db as a float data type
     close_price = float(data.closeprice)
     stock_name = str(data.name)                                     #Strips and assigns the name value as a string data type
     ticker = str(data.ticker)                                       #Strips and assigns the ticker as a string
-    wallet_balance = float(balance.balance)                         #Assigns the wallet balance as a float data type   
+    amount1 = float(balances.balance)                         #Assigns the wallet balance as a float data type   
     
     
     if request.method == "POST":
@@ -276,17 +341,20 @@ def short_position(request):
             
             sum_price = float(Amount.total_price)                                   # amount user wants to trade
             otp = Amount.otp                                                        # otp code
-            leverage = Amount.leverage                                              # Leverage used          
+            leverage_used = Amount.leverage                                              # Leverage used          
+            Trading_instance = Trading(amount1, leverage_used)
             
             if totp.verify(otp):
-                wallet = Wallet.objects.filter(user=user_name).last()
-                create_trade(wallet, user_name, stock_name, ticker, leverage, sum_price, close_price, open_price, balance, sign)
+                Short.objects.create(user = user_name, name = stock_name, amount = sum_price, ticker = ticker, 
+                    leverage = leverage_used, current_price = close_price, short_price = open_price, 
+                    returns = Trading_instance.short_position(sum_price, open_price, close_price))
                 
-                wallet.save()
-                Amount.delete()
+                balances.balance -= sum_price
+                balances.save()
+
                 data.delete()
                 return redirect('payment:done')
             else:
                 return redirect('payment:canceled')
         
-    return render(request, 'stock_short.html', {'Sell': Sell, 'form': form,  'total': wallet_balance, 'prices': open_price, 'stock': stock_name })
+    return render(request, 'stock_short.html', {'Sell': Sell, 'form': form,  'total': balances.balance, 'prices': open_price, 'stock': stock_name })
